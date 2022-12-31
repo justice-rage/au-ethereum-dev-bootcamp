8d0820fd9041f2474cab6d5169f865efff6314ba
justicepelteir <justice.douglas@hey.com>
2022-12-30 19:35:57 -0800

    complete 3: block hash

---
+++ week-1/proof-of-work/proof-of-work-miner/3-block-hash/index.js
@@ -0,0 +1,31 @@
+const SHA256 = require('crypto-js/sha256');
+const TARGET_DIFFICULTY = BigInt(0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+const MAX_TRANSACTIONS = 10;
+
+const mempool = [];
+const blocks = [];
+
+function addTransaction(transaction) {
+    // TODO: add transaction to mempool
+    mempool.push(transaction);
+}
+
+function mine() {
+    // TODO: mine a block
+    const block = {
+        id: blocks.length
+    }
+    const stringifyBlock = JSON.stringify(block);
+    const blockSHA256 = SHA256(stringifyBlock);
+    block.hash = blockSHA256;
+    blocks.push(block);
+}
+
+module.exports = {
+    TARGET_DIFFICULTY,
+    MAX_TRANSACTIONS,
+    addTransaction, 
+    mine, 
+    blocks,
+    mempool
+};
